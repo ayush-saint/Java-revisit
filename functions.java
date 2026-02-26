638afbc17bcd8c808d336bcb6249f2f0219d7700
ayush-saint <ayush22kumar37@gamil.com>
2026-02-26 11:08:44 +0530

    Funcations/Methods in java

---
+++ functions.java
@@ -0,0 +1,21 @@
+/*       Function in java is called method :
+Return Type, Function Name, Input Type, Input Name and Input Order       */
+
+public class functions {
+
+    public static void main(String[] args) {
+        int result = add(5, 3);
+        System.out.println("Sum = " + result);
+    }
+    
+
+    // Simple function
+    public static int add(int a, int b) {
+        return a + b;
+
+    }
+}
+
+    
+
+
